daa01f830d286b0e1d690426683f86f2b3c6e10c
assonif <fabioassoni06@gmail.com>
2020-08-21 20:22:52 -0300

    test

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Container } from './styles';
 
 const Login: React.FC = () => {
-  return <Container>logisns</Container>;
+  return <Container>slogisns</Container>;
 };
 
 export default Login;
